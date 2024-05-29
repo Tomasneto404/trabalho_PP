4750c85591d7685e13ae6178df949ff67f1ffb95
LAPTOP-RBJLTLUV\Asus\tania <8220190@estg.ipp.pt>
2024-05-29 11:59:30 +0100

    measurement class

---
+++ trabalho_PP/src/PP_AC_8220190_8220862/core/Measurement.java
@@ -0,0 +1,56 @@
+/*
+* Nome: Tomás Leonardo Leão Sousa Neto
+* Número: 8220862
+* Turma: LSIRC12T1
+*
+* Nome: Tânia Sofia da Silva Morais
+* Número: 8220190
+* Turma: LSIRC12T1
+ */
+package PP_AC_8220190_8220862.classes;
+
+import java.time.LocalDateTime;
+
+/**
+ * <strong>AidBox</strong>
+ * <p>
+ * This class intends to implement the measurements of a count on a given date
+ * </p>
+ *
+ */
+public class Measurement implements com.estg.core.Measurement {
+
+    private LocalDateTime date;
+
+    private double value;
+
+    /**
+     *  <strong> Measurement()</strong>
+     * <p>Measurement constructor method.</p>
+     * @param value receives the container weight in kg
+     */
+    public Measurement(double value) {
+        this.value = value;
+    }
+
+    /**
+     * <strong> getDate() </strong>
+     * <p> getter corresponding to the weight insertion date </p>
+     * @return the weight insertion date
+     */
+    @Override
+    public LocalDateTime getDate() {
+        return this.date;
+    }
+
+    /**
+     * <strong> getValue() </strong>
+     * <p> weight getter in kg </p>
+     * @return the weight of the container in kg
+     */
+    @Override
+    public double getValue() {
+        return this.value;
+    }
+
+}
