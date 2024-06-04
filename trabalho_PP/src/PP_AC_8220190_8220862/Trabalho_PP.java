a1c99c25ffd3a71d7cd34a39a559a40a0ea22ef4
Tomasneto404 <tomasneto26@gmail.com>
2024-06-04 11:21:04 +0100

    testes

--- trabalho_PP/src/PP_AC_8220190_8220862/Trabalho_PP.java
+++ trabalho_PP/src/PP_AC_8220190_8220862/Trabalho_PP.java
@@ -14,17 +14,24 @@ import PP_AC_8220190_8220862.core.GeographicCoordinates;
 import PP_AC_8220190_8220862.core.Container;
 import com.estg.core.ItemType;
 import com.estg.core.exceptions.ContainerException;
+import com.estg.io.HTTPProvider;
 
 
 public class Trabalho_PP {    
     
     public static void main(String[] args) throws ContainerException{
+        HTTPProvider provider = new HTTPProvider();
+        
         AidBox box = new AidBox("123", "lixa");
     
         Container container = new Container("3232", 323, ItemType.CLOTHING);
 
-        System.out.println(box.addContainer(container));
-        System.out.println(box.getContainers());
+        //System.out.println(box.addContainer(container));
+        //System.out.println(box.getContainers());
+        
+        String string = provider.getFromURL("https://data.mongodb-api.com/app/data-docuz/endpoint/aidboxesbyid?codigo=CAIXF37");
+        
+        System.out.println(string);
     }
     
 }
