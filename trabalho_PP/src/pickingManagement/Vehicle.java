8a064a370b42f653bf1cffe4b0ead4dcd709b34a
LAPTOP-RBJLTLUV\Asus\tania <8220190@estg.ipp.pt>
2024-06-06 15:01:19 +0100

    ll

--- trabalho_PP/src/pickingManagement/Vehicle.java
+++ trabalho_PP/src/pickingManagement/Vehicle.java
@@ -1,14 +1,54 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+* Nome: Tomás Leonardo Leão Sousa Neto
+* Número: 8220862
+* Turma: LSIRC12T1
+*
+* Nome: Tânia Sofia da Silva Morais
+* Número: 8220190
+* Turma: LSIRC12T1
  */
 package pickingManagement;
 
+import com.estg.core.exceptions.VehicleException;
+import com.estg.core.ItemType;
+
 /**
- *
- * @author Asus
+ * <strong>Vehicle</strong>
+ * <p>
+ * This class identifies a vehicle</p>
  */
-public class Vehicle {
-    
+public class Vehicle implements com.estg.pickingManagement.Vehicle {
+
+    private ItemType item;
+
+    private double maxCapacity;
+
+    /**
+     * <strong>Vehicle() </strong>
+     * <p> contructor method </p>
+     * @param item receives a variable of ItemType type
+     * @param maxCapacity receives a variable of double type
+     * @throws VehicleException exception corresponding to a vehicle
+     */
+    public Vehicle(ItemType item, double maxCapacity) throws VehicleException {
+
+        if (item == ItemType.PERISHABLE_FOOD) {
+            throw new VehicleException("This type of item is not possible");
+        }
+
+        this.item = item;
+
+        this.maxCapacity = maxCapacity;
+    }
+
+    @Override
+    public ItemType getSupplyType() {
+        return this.item;
+    }
+
+    @Override
+    public double getMaxCapacity() {
+        return this.maxCapacity;
+    }
+
 }
