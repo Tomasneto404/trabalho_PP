164075b956ac3f18b0a3118d3e05179a75b418a9
LAPTOP-RBJLTLUV\Asus\tania <8220190@estg.ipp.pt>
2024-06-07 18:48:22 +0100

    ja da

--- trabalho_PP/src/PP_AC_8220190_8220862/core/InstitutionImp.java
+++ trabalho_PP/src/PP_AC_8220190_8220862/core/InstitutionImp.java
@@ -19,56 +19,60 @@ import com.estg.pickingManagement.PickingMap;
 
 import java.time.LocalDateTime;
 
-import PP_AC_8220190_8220862.pickingManagement.Vehicle;
 
+import PP_AC_8220190_8220862.pickingManagement.Vehicle;
 /**
  *
  * @author tomas
  */
-public class Institution implements com.estg.core.Institution {
+public class InstitutionImp implements com.estg.core.Institution {
 
     private final int MAX_AIDBOXS = 10;
-
+    
     private final int MAX_MEASUREMENTS = 10;
-
+    
     private final int MAX_VEHICLES = 10;
-
+    
+    private final int MAX_PICKINGMAPS = 10;
+    
     private String name;
-
+    
     private AidBox[] aidBoxs;
-
+    
     private int aidBoxCounter;
-
+    
     private Measurement[] measurements;
-
+    
     private int measurementCounter;
-
+    
     private Vehicle[] vehicles;
-
+    
     private int vehicleCounter;
-
+    
+    private PickingMap[] pickingMaps;
+    
+    private int pickingMpaCounter;
+    
     /**
      * <strong>Institution()</strong>
-     * <p>
-     * Constructor method of Institution</p>
-     *
+     * <p>Constructor method of Institution</p>
      * @param name Receives the name of the institution.
      */
-    public Institution(String name) {
+    public InstitutionImp(String name) {
         this.name = name;
         this.aidBoxCounter = 0;
         this.vehicleCounter = 0;
         this.measurementCounter = 0;
+        this.pickingMpaCounter = 0;
         this.aidBoxs = new AidBox[MAX_AIDBOXS];
         this.measurements = new Measurement[MAX_MEASUREMENTS];
         this.vehicles = new Vehicle[MAX_VEHICLES];
+        this.pickingMaps = new PickingMap[MAX_PICKINGMAPS];
     }
-
+    
     /**
      * <strong>getName()</strong>
-     * <p>
-     * This method returns a string with the name of the institution.</p>
-     *
+     * <p>This method returns a string with the name of the institution.</p>
      * @return name
      */
     @Override
@@ -78,42 +82,37 @@ public class Institution implements com.estg.core.Institution {
 
     /**
      * <strong>addAidBox()</strong>
-     * <p>
-     * This method inserts and aidBox to the institution aidbox array.</p>
-     *
+     * <p>This method inserts and aidBox to the institution aidbox array.</p>
      * @param aidbox
      * @return true if it was possible to insert the new aidBox.
-     * @throws AidBoxException
+     * @throws AidBoxException 
      */
     @Override
     public boolean addAidBox(AidBox aidbox) throws AidBoxException {
-
-        if (!canAddAidBoxToArray()) {
+        
+        if ( !canAddAidBoxToArray() ) {
             throw new AidBoxException("AidBox array is full.");
         }
-
-        if (verifyAidBox(aidbox)) {
+        
+        if ( verifyAidBox(aidbox) ) {
             throw new AidBoxException("This AidBox already exists in the array.");
-        }
-
+        }             
+                
         this.aidBoxs[this.aidBoxCounter++] = aidbox;
-
+        
         return true;
     }
-
+    
     /**
      * <strong>verifyAidBox()</strong>
-     * <p>
-     * This method verifys if there is and equal aidbox to the one to be
-     * inserted in the array</p>
-     *
+     * <p>This method verifys if there is and equal aidbox to the one to be inserted in the array</p>
      * @param adbx
      * @return True if already exists the same aindabox. false if it doesn't.
      */
     private boolean verifyAidBox(AidBox adbx) {
         for (AidBox aidbox : this.aidBoxs) {
 
-            if (aidbox.equals(adbx)) {
+            if ( aidbox.equals(adbx) ) {
 
                 return true;
 
@@ -123,13 +122,10 @@ public class Institution implements com.estg.core.Institution {
 
         return false;
     }
-
+    
     /**
      * <strong>canAddAidBoxToArray()</strong>
-     * <p>
-     * This method verifys if it's possible to fit a new aidBox to the
-     * array.</p>
-     *
+     * <p>This method verifys if it's possible to fit a new aidBox to the array.</p>
      * @return True if there is space to insert a new aidBox.
      */
     private boolean canAddAidBoxToArray() {
@@ -138,67 +134,58 @@ public class Institution implements com.estg.core.Institution {
 
     /**
      * <strong>addMeasurement()</strong>
-     * <p>
-     * This method makes verifications and inserts a new Measurement according
-     * to a Container in the measurements array of the Institution.</p>
-     *
+     * <p>This method makes verifications and inserts a new Measurement according to a Container in the measurements array of the Institution.</p>
      * @param msrmnt Measurement object to be inserted
      * @param cntnr Container of the measurement
      * @return true if it was possible to add the new Measurement to the array.
      * @throws ContainerException
-     * @throws MeasurementException
+     * @throws MeasurementException 
      */
     @Override
     public boolean addMeasurement(Measurement msrmnt, Container cntnr) throws ContainerException, MeasurementException {
-
-        if (!canAddMeasurementToArray()) {
+        
+        if ( !canAddMeasurementToArray() ) {
             throw new MeasurementException("Measurements array is full");
         }
-
-        if (verifyMeasurement(msrmnt)) {
-            throw new MeasurementException("This Measurement already exists in the array.");
+        
+        if ( verifyMeasurement(msrmnt) ) {
+             throw new MeasurementException("This Measurement already exists in the array.");
         }
-
-        if (verifyContainerMeasurements(msrmnt, cntnr)) {
+        
+        if ( verifyContainerMeasurements(msrmnt, cntnr) ) {
             throw new ContainerException("Container already has a measurement with the same date.");
         }
-
+        
         this.measurements[measurementCounter++] = msrmnt;
-
+        
         return true;
     }
-
+    
     /**
      * <strong>verifyContainerMeasurements()</strong>
-     * <p>
-     * This method verifys if a given container has in its Measurements array a
-     * measurement with the same date as the one to be verified.</p>
-     *
+     * <p>This method verifys if a given container has in its Measurements array a measurement with the same date as the one to be verified.</p>
      * @param msrmnt Measurement to verify date
-     * @param cntnr Container to check if already exists a Measurement with the
-     * date of the measurement received.
-     * @return True if there is a measurement with the same date. False if it
-     * doesn't.
+     * @param cntnr Container to check if already exists a Measurement with the date of the measurement received.
+     * @return True if there is a measurement with the same date. False if it doesn't.
      */
     private boolean verifyContainerMeasurements(Measurement msrmnt, Container cntnr) {
-
-        return cntnr.getMeasurements(msrmnt.getDate().toLocalDate()).length != 0;
+        
+       return cntnr.getMeasurements( msrmnt.getDate().toLocalDate() ).length != 0;
     }
-
-    /**
+    
+    
+    
+   /**
      * <strong>verifyMeasurement()</strong>
-     * <p>
-     * This method verifys if there is and equal measurement to the one to be
-     * inserted in the array</p>
-     *
+     * <p>This method verifys if there is and equal measurement to the one to be inserted in the array</p>
      * @param msrmnt
      * @return True if already exists the same measurement. false if it doesn't.
      */
     private boolean verifyMeasurement(Measurement msrmnt) {
-
+        
         for (Measurement measurement : this.measurements) {
 
-            if (measurement.equals(msrmnt)) {
+            if ( measurement.equals(msrmnt) ) {
 
                 return true;
 
@@ -208,24 +195,19 @@ public class Institution implements com.estg.core.Institution {
 
         return false;
     }
-
+    
     /**
      * <strong>canAddMeasurementToArray()</strong>
-     * <p>
-     * This method verifys if it's possible to fit a new Measurement Object to
-     * the array.</p>
-     *
+     * <p>This method verifys if it's possible to fit a new Measurement Object to the array.</p>
      * @return True if there is space to insert a new Measurement.
      */
-    private boolean canAddMeasurementToArray() {
+    private boolean canAddMeasurementToArray(){
         return this.measurementCounter < this.measurements.length;
     }
 
     /**
      * <strong>getAidBoxes()</strong>
-     * <p>
-     * This method gets the aidBoxs array of the institution..</p>
-     *
+     * <p>This method gets the aidBoxs array of the institution..</p>
      * @return array with aidboxes of the institution.
      */
     @Override
@@ -235,30 +217,25 @@ public class Institution implements com.estg.core.Institution {
 
     /**
      * <strong>getContainer()</strong>
-     * <p>
-     * This method gets a container with a specific ItemType from a given
-     * aidBox</p>
-     *
+     * <p>This method gets a container with a specific ItemType from a given aidBox</p>
      * @param aidbox Aid box to get the container
      * @param it The specific Item type.
      * @return The wanted container
-     * @throws ContainerException If Container was not found.
+     * @throws ContainerException If Container was not found. 
      */
     @Override
     public Container getContainer(AidBox aidbox, ItemType it) throws ContainerException {
-
+        
         if (aidbox.getContainer(it) == null) {
-            throw new ContainerException("Container with item type " + it + "doesn't exist in this AidBox.");
+             throw new ContainerException("Container with item type " + it + "doesn't exist in this AidBox.");
         }
-
+        
         return aidbox.getContainer(it);
     }
 
     /**
      * <strong>getVehicles()</strong>
-     * <p>
-     * This method get the Vehicles array of the institution.</p>
-     *
+     * <p>This method get the Vehicles array of the institution.</p>
      * @return Array with the vehicles.
      */
     @Override
@@ -268,41 +245,33 @@ public class Institution implements com.estg.core.Institution {
 
     /**
      * <strong>addVehicle()</strong>
-     * <p>
-     * This method adds a new vehicle to the array of vehicles.</p>
-     *
+     * <p>This method adds a new vehicle to the array of vehicles.</p>
      * @param vhcl Vehicle to be inserted.
      * @return True if it was possible to insert the new vehicle.
-     * @throws VehicleException If ocurred and error during the validations to
-     * insert the new vehicle.
+     * @throws VehicleException If ocurred and error during the validations to insert the new vehicle.
      */
     @Override
     public boolean addVehicle(com.estg.pickingManagement.Vehicle vhcl) throws VehicleException {
-
-        Vehicle vehicle= (Vehicle) vhcl;
         
-        if (!canAddVehicleToArray()) {
+        if ( !canAddVehicleToArray() ) {
             throw new VehicleException("Vehicle array is full.");
         }
-
-        if (verifyVehicle(vehicle)) {
+        
+        if ( verifyVehicle((Vehicle) vhcl) ) {
             throw new VehicleException("This vehicle already exists inside the array.");
         }
-
-        this.vehicles[this.vehicleCounter++] = vehicle;
-
+     
+        this.vehicles[this.vehicleCounter++] = (Vehicle) vhcl;
+        
         return true;
     }
-
+    
     /**
      * <strong>canAddvehicleToArray()</strong>
-     * <p>
-     * This method verifys if it's possible to fit a new vehicle to the
-     * array.</p>
-     *
+     * <p>This method verifys if it's possible to fit a new vehicle to the array.</p>
      * @return True if it was possible to insert the new vehicle.
      */
-    private boolean canAddVehicleToArray() {
+    private boolean canAddVehicleToArray(){
         return this.vehicleCounter < this.vehicles.length;
     }
 
@@ -316,10 +285,10 @@ public class Institution implements com.estg.core.Institution {
      * @return true if already exists an equal vehicle, false if it doesn't
      */
     private boolean verifyVehicle(Vehicle vhcl) {
-
+        
         for (Vehicle vehicle : this.vehicles) {
 
-            if (vehicle.equals(vhcl)) {
+            if ( vehicle.equals(vhcl) ) {
 
                 return true;
 
@@ -329,22 +298,26 @@ public class Institution implements com.estg.core.Institution {
 
         return false;
     }
-
+    
+    
     @Override
-    public void disableVehicle(com.estg.pickingManagement.Vehicle vhcl) throws VehicleException {
-        Vehicle vehicle= (Vehicle) vhcl;
-        vehicle.setState(VehicleState.INACTIVE);
+    public void disableVehicle(com.estg.pickingManagement.Vehicle vhcl) throws VehicleException {        
+        //vhcl.setState(VehicleState.INACTIVE);
     }
 
     @Override
     public void enableVehicle(com.estg.pickingManagement.Vehicle vhcl) throws VehicleException {
-        Vehicle vehicle= (Vehicle) vhcl;
-        vehicle.setState(VehicleState.ACTIVE);
+        //vhcl.setState(VehicleState.ACTIVE);
     }
 
+    /**
+     * <strong>getPickingMaps()</strong>
+     * <p>This method gets the pickingMaps array.</p>
+     * @return The PickingMaps array.
+     */
     @Override
     public PickingMap[] getPickingMaps() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.pickingMaps;
     }
 
     @Override
@@ -352,9 +325,22 @@ public class Institution implements com.estg.core.Institution {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     * <strong>getCurrentPickingMap()</strong>
+     * <p>This method returns the current picking map that is being used by the Institution</p>
+     * @return The last PickingMap from the array of pickingMaps;
+     * @throws PickingMapException If the PickingMap doesn't exist.
+     */
     @Override
     public PickingMap getCurrentPickingMap() throws PickingMapException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        PickingMap lastPckMp = this.pickingMaps[this.pickingMpaCounter];
+        
+        if (lastPckMp == null) {
+            throw new PickingMapException("No current PickingMap is being used.");
+        }
+        
+        return lastPckMp;
     }
 
     @Override
@@ -366,5 +352,7 @@ public class Institution implements com.estg.core.Institution {
     public double getDistance(AidBox aidbox) throws AidBoxException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
+    
+    
+    
 }
