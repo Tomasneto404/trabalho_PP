63c1b4bdd0244f5562a80a27d8e1875435433683
LAPTOP-RBJLTLUV\Asus\tania <8220190@estg.ipp.pt>
2024-06-07 16:04:56 +0100

    acho que os veículos refrigerados é só isso

--- trabalho_PP/src/PP_AC_8220190_8220862/pickingManagement/RefrigereratedVehicle.java
+++ trabalho_PP/src/PP_AC_8220190_8220862/pickingManagement/RefrigereratedVehicle.java
@@ -1,7 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+* Nome: Tomás Leonardo Leão Sousa Neto
+* Número: 8220862
+* Turma: LSIRC12T1
+*
+* Nome: Tânia Sofia da Silva Morais
+* Número: 8220190
+* Turma: LSIRC12T1
  */
 package PP_AC_8220190_8220862.pickingManagement;
 
@@ -9,26 +13,56 @@ import com.estg.core.ItemType;
 import com.estg.core.exceptions.VehicleException;
 
 /**
- *
- * @author Asus
+ * <strong>Vehicle</strong>
+ * <p>
+ * This class identifies a vehicle</p>
  */
 public class RefrigereratedVehicle extends Vehicle {
-    
+
+    private double maxKilometers;
+
     private ItemType item;
-    
-    
-    public RefrigereratedVehicle(String plate, double maxCapacity) {
+
+    /**
+     * <strong> RegrigereratedVehicle() </strong>
+     * <p>
+     * constructor method </p>
+     *
+     * @param plate variable of String type
+     * @param maxCapacity variable of double type
+     * @param maxKilometers variable of double type
+     */
+    public RefrigereratedVehicle(String plate, double maxCapacity, double maxKilometers) {
         super(plate, maxCapacity);
+        this.maxKilometers = maxKilometers;
     }
-    
+
+    /**
+     * <strong> getMaxKilometers() </strong>
+     * <p>
+     * getters corresponding to a max kilometers </p>
+     *
+     * @return the refrigererated vehicle's max kilometers
+     */
+    public double getMaxKilometers() {
+        return this.maxKilometers;
+    }
+
+    /**
+     * <strong> setItem() </strong>
+     * <p>
+     * setter corresponding to a item, it verifies if the item type equal to
+     * perishable food </p>
+     *
+     * @param item variable of ItemType type
+     * @throws VehicleException excepction corresponding a vehicle
+     */
     @Override
-     public void setItem(ItemType item) throws VehicleException {
-        if (item != ItemType.PERISHABLE_FOOD) { 
+    public void setItem(ItemType item) throws VehicleException {
+        if (item != ItemType.PERISHABLE_FOOD) {
             throw new VehicleException("Wrong type");
         }
         this.item = item;
     }
-     
-     
-    
+
 }
