ddd16acb2749c44c98fbacf32afa13e5ab9003c4
LAPTOP-RBJLTLUV\Asus\tania <8220190@estg.ipp.pt>
2024-06-10 19:08:59 +0100

    vehicle state

--- trabalho_PP/src/PP_AC_8220190_8220862/enums/VehicleState.java
+++ trabalho_PP/src/PP_AC_8220190_8220862/enums/VehicleState.java
@@ -1,12 +1,17 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+* Nome: Tomás Leonardo Leão Sousa Neto
+* Número: 8220862
+* Turma: LSIRC12T1
+*
+* Nome: Tânia Sofia da Silva Morais
+* Número: 8220190
+* Turma: LSIRC12T1
  */
 package PP_AC_8220190_8220862.enums;
 
 /**
- *
+ *<strong>  VehicleState </strong>
+ *<p> enum of a vehicle state </p>
  * @author tomas
  */
 public enum VehicleState {
