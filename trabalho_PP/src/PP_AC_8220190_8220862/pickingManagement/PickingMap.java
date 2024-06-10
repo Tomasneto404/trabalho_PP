3a96d9fbee8cfc1864261fc9f8a926eb0d53210e
LAPTOP-RBJLTLUV\Asus\tania <8220190@estg.ipp.pt>
2024-06-10 19:13:01 +0100

    Pickingmap documentado

--- trabalho_PP/src/PP_AC_8220190_8220862/pickingManagement/PickingMap.java
+++ trabalho_PP/src/PP_AC_8220190_8220862/pickingManagement/PickingMap.java
@@ -1,7 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+* Nome: Tomás Leonardo Leão Sousa Neto
+* Número: 8220862
+* Turma: LSIRC12T1
+*
+* Nome: Tânia Sofia da Silva Morais
+* Número: 8220190
+* Turma: LSIRC12T1
  */
 package PP_AC_8220190_8220862.pickingManagement;
 
@@ -9,8 +13,9 @@ import com.estg.pickingManagement.Route;
 import java.time.LocalDateTime;
 
 /**
- *
- * @author tomas
+ * <strong> PickingMap </strong>
+ * <p>
+ * This class identifies a picking map </p>
  */
 public class PickingMap implements com.estg.pickingManagement.PickingMap {
 
@@ -20,16 +25,31 @@ public class PickingMap implements com.estg.pickingManagement.PickingMap {
 
     private Route[] routes;
 
+    /**
+     * <strong> PickingMap() </strong>
+     * <p>
+     * PickingMap constructor method </p>
+     */
     public PickingMap() {
         this.date = LocalDateTime.now();
         this.routes = new Route[MAX_ROUTES];
     }
 
+    /**
+     * <strong> getDate() </strong>
+     * <p> obtains a date of a picking map </p>
+     * @return the date of a picking map
+     */
     @Override
     public LocalDateTime getDate() {
         return this.date;
     }
 
+    /**
+     * <strong> getRoutes() </strong>
+     * <p> obtains the route values </p>
+     * @return the route values of a picking map
+     */
     @Override
     public Route[] getRoutes() {
         return this.routes;
